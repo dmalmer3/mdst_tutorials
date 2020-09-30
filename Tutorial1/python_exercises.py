0540d5c4fb9d92f73a9b84f1bba96c4474ce40a5
dmalmer3 <52764845+dmalmer3@users.noreply.github.com>
2020-09-30 00:47:21 -0400

    Update python_exercises.py

--- Tutorial1/python_exercises.py
+++ Tutorial1/python_exercises.py
@@ -6,13 +6,25 @@ def is_odd(x):
     """
     returns True if x is odd and False otherwise
     """
+    if (x % 2 == 0):
+        return True
+    
+    return False
 
+print(is_odd(2))
+print(is_odd(3))
 
 def is_palindrome(word):
     """
     returns whether `word` is spelled the same forwards and backwards
     """
+    if (word == word[::-1]):
+        return True
+   
+    return False
 
+print(is_palindrome('level'))
+print(is_palindrome('word'))
 
 def only_odds(numlist):
     """
@@ -20,7 +32,19 @@ def only_odds(numlist):
 
     ex: only_odds([1, 2, 3, 4, 5, 6]) -> [1, 3, 5]
     """
+    oddlist = []
+    
+    for i in numlist:
+        if i % 2 == 0:
+            pass
+        else:
+            oddlist.append(i)
+    
+    return oddlist
 
+print(only_odds([1,2,3,4,5,6,7,8,9]))
+    
+            
 
 def count_words(text):
     """
@@ -35,3 +59,20 @@ def count_words(text):
         {'how': 1, 'much': 1, 'wood': 1, 'would': 1, 'a': 2, 'woodchuck': 2,
         'chuck': 2, 'if': 1, 'could': 1, 'wood?': 1}
     """
+    words = text.split()
+    lower_words = []
+    for word in words:
+        lower_word = word.lower()
+        lower_words.append(lower_word)
+    words_dict = dict.fromkeys(lower_words)
+    for i in words_dict.keys():
+        counter = 0
+        for j in lower_words:
+            if i == j:
+                counter += 1
+                
+        words_dict[i] = counter
+    
+    return words_dict
+print(count_words("How much wood would a woodchuck chuck"
+                    " if a woodchuck could chuck wood?"))
